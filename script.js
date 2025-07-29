506bd0a0e59427779991729aa82a288eff2e18ae
benalak11 <mm8584856@gmail..com>
2025-07-29 16:13:33 +0100

    task 0

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('registration-form');
+    form.addEventListener('sumbit', function(event {
+        event.preventDefault();
+    }
+    document.getElementById('form-feedback');
+})
