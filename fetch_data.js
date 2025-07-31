635831d1e0afa6b6289e73a27005d1d2da62d8e2
benalak11 <mm8584856@gmail..com>
2025-07-31 23:17:45 +0100

    task 3

---
+++ fetch_data.js
@@ -0,0 +1,8 @@
+function fetchUserData() {
+  // Function body
+  try {
+  const apiUrl = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json() 
+const dataContainer = document.getElementById("api-data");
+}
+}
