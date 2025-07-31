a5773a288e5be3fa0d16e0da2e4547c220fd19bb
benalak11 <mm8584856@gmail..com>
2025-07-31 16:28:49 +0100

    task 0

--- script.js
+++ script.js
@@ -5,7 +5,27 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     document.getElementById('form-feedback');
     const usernameInput = document.getElementById('username');
+    const usernameError = document.getElementById('usernameError');
+      if (usernameInput.value.length < 3) {
+        usernameError.textContent = 'Username must be at least 3 characters long.';
+        return;
+      } else {
+        usernameError.textContent = '';
+      }
     const usernameInput = document.getElementById('email');
     const usernameInput = document.getElementById('password');
     Input.value = Input.value.trim();
+
+    const validationInput = () => {
+        const usernameValue = username.value.trim();
+        const emailValue = email.value.trim();
+        const passwordValue = password.value.trim();
+        if usernameValue == '' {
+            setError(username, 'message');
+        }
+        else {
+
+        }
+    }
+
 })
